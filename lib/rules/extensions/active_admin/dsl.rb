608b3c3573a50e493d1236301e1aed4e60954051
twistir <twistir123@gmail.com>
2019-04-02 10:27:36 -0400

    Update dsl.rb

--- lib/rules/extensions/active_admin/dsl.rb
+++ lib/rules/extensions/active_admin/dsl.rb
@@ -18,6 +18,27 @@ ActiveAdmin::FormBuilder.class_eval do
   end
 end
 
+    
+FormBuilder.class_eval do
+  def has_rules
+    inputs 'Rules' do
+      semantic_fields_for :rule_set do |rules_rule_set_form|
+        rules_rule_set_form.has_many :rules do |rules_rule_form|
+          rules_rule_form.input :lhs_parameter_key, :label => 'Left hand side', collection: rules_parameter_collection(object.rule_set)
+          rules_rule_form.input :evaluator_key, :label => 'Evaluator', :as => :select, :collection => Rules.evaluators.map {|key, evaluator| [evaluator.name, key, {:'data-requires-rhs' => evaluator.requires_rhs?}] }.sort_by {|name, key| name }
+          rules_rule_form.input :rhs_parameter_raw, :label => 'Enter a value', :wrapper_html => { :class => "rules_rhs_parameter" }
+          rules_rule_form.input :rhs_parameter_key, :label => 'Or choose a value', collection: rules_parameter_collection(object.rule_set), :wrapper_html => { :class => "rules_rhs_parameter" }
+        end
+        rules_rule_set_form.input :evaluation_logic, :as => :select, :label => 'Must match', collection: [['All Rules', 'all'], ['Any Rules', 'any']]
+      end
+    end
+  end
+
+  def rules_parameter_collection(rule_set)
+    @rules_parameter_collection ||= Rules.constants.merge(rule_set.try(:attributes) || {}).map {|key, const| [const.name, key, {:'data-type' => const.try(:type) || 'string'}] }
+  end
+end
+
 ActiveAdmin::Views::Pages::Show.class_eval do
   def show_rules
     panel "Rules" do
